7cb7d4c97f94178e65d21fbf3fbebbeccb4edac0
DavidDanso <desiingerboy@icloud.com>
2024-04-16 14:08:54 +0000

    added URL configuration for the 'quote' app

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('quotes/', include('quote.urls')),
+    path('api/quotes/', include('quote.urls')),
 ]
--- quote/views.py
+++ quote/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from .models import Quote
 from .serializers import QuoteSerializer
 
-# Create your views here.
+# endpoint to get all quotes
 @api_view(['GET'])
 def get_quotes(request):
     quotes = Quote.objects.all()
@@ -12,6 +12,7 @@ def get_quotes(request):
     return Response(data)
 
 
+# endpoint to get random quote
 @api_view(['GET'])
 def random_quotes(request):
     quote = Quote.objects.all().order_by('?').first()
